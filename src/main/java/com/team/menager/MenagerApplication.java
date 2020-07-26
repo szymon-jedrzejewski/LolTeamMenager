d6fe8ba3fbecb3f4d7c95a5b05eb8dcc9566e838
Szymon Jedrzejewski <szymon219@poczta.fm>
2020-07-26 13:11:25 +0200

    Initial commit

---
+++ src/main/java/com/team/menager/MenagerApplication.java
@@ -0,0 +1,13 @@
+package com.team.menager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/menager/MenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.menager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
