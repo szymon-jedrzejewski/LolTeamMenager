0be2aa27475774a9152a6e3e3edbc37c2cf1c2c0
Szymon Jedrzejewski <szymonj219@gmail.com>
2020-08-03 16:03:21 +0200

    Fix typo

--- src/main/java/com/team/menager/ManagerApplication.java
+++ src/main/java/com/team/menager/ManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MenagerApplication {
+public class ManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MenagerApplication.class, args);
+        SpringApplication.run(ManagerApplication.class, args);
     }
 
 }
