cd5472a1deda4cf59cd376d4ff7dc433cded5231
Szymon Jedrzejewski <szymonj219@gmail.com>
2020-08-04 16:20:15 +0200

    Create necessary packages

--- src/main/java/com/team/manager/controller/FirstController.java
+++ src/main/java/com/team/manager/controller/FirstController.java
@@ -1,4 +1,4 @@
-package com.team.menager;
+package com.team.manager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/team/manager/ManagerApplicationTests.java
+++ src/test/java/com/team/manager/ManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.team.menager;
+package com.team.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
