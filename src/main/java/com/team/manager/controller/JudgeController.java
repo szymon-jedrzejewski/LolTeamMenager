d398a196cb94fa22c659617bb5a70aaadeb840c7
Szymon Jedrzejewski <szymonj219@gmail.com>
2020-08-21 18:20:13 +0200

    Add necessary methods for 'JudgeController'

--- src/main/java/com/team/manager/controller/JudgeController.java
+++ src/main/java/com/team/manager/controller/JudgeController.java
@@ -1,9 +1,14 @@
 package com.team.manager.controller;
 
+import com.team.manager.entity.Player;
 import com.team.manager.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/judge")
@@ -14,5 +19,20 @@ public class JudgeController {
     @Autowired
     public JudgeController(PlayerService playerService) {
         this.playerService = playerService;
+    }
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return playerService.getPlayers();
+    }
+
+    @GetMapping("/playerById/{id}")
+    public Player getPlayerById(@PathVariable int id) {
+        return playerService.getPlayerById(id);
+    }
+
+    @GetMapping("/player/{nick}")
+    public Player getPlayerByNick(@PathVariable String nick) {
+        return playerService.getPlayerByNick(nick);
     }
 }
