f619fae59aa76cdadb07d9e11e599afa33cae259
Tomasz Dubiel <marder102@gmail.com>
2020-08-27 14:50:06 +0200

    fix naming, added tests for class Match

--- src/main/java/com/team/manager/engine/Match.java
+++ src/main/java/com/team/manager/engine/Match.java
@@ -10,10 +10,10 @@ public class Match {
 
     PlayerStats stats = new PlayerStats();
 
-    public int lanerComparison(Player a, Player b){
-        int score=0;
-        PlayerStats playerA=a.getPlayerStats(),playerB=b.getPlayerStats();
-        List<Integer> playerAStats = new LinkedList<>(),playerBStats = new LinkedList<>();
+    public int comparePlayerStats(Player a, Player b) {
+        int score = 0;
+        PlayerStats playerA = a.getPlayerStats(), playerB = b.getPlayerStats();
+        List<Integer> playerAStats = new LinkedList<>(), playerBStats = new LinkedList<>();
 
         playerAStats.add(playerA.getLaningPhase());
         playerAStats.add(playerA.getMapAwareness());
@@ -27,8 +27,8 @@ public class Match {
         playerBStats.add(playerB.getPositioning());
         playerBStats.add(playerB.getTeamfighting());
 
-        for(int i=0; i<5; i++){
-            if(playerAStats.get(i)>playerBStats.get(i)){
+        for (int i = 0; i < 5; i++) {
+            if (playerAStats.get(i) > playerBStats.get(i)) {
                 score++;
             }
         }
@@ -36,23 +36,20 @@ public class Match {
         return score;
     }
 
-    public String matchSimulation(List<Player> a, List<Player> b){
-        int scoreA=0,scoreB=0; String outcome;
-        for(int i=0; i<5; i++){
-            scoreA=scoreA+lanerComparison(a.get(i),b.get(i));
-            scoreB=scoreB+lanerComparison(b.get(i),a.get(i));
+    public int matchResult(List<Player> teamA, List<Player> teamB) {
+        int scoreA = 0, scoreB = 0;
+        for (int i = 0; i < 5; i++) {
+            scoreA = scoreA + comparePlayerStats(teamA.get(i), teamB.get(i));
+            scoreB = scoreB + comparePlayerStats(teamB.get(i), teamA.get(i));
         }
-        System.out.println("Team A score: "+scoreA+" Team B score: "+scoreB);
-        if(scoreA>scoreB){
-            outcome="Team A won";
+        System.out.println("Team A score: " + scoreA + " Team B score: " + scoreB);
+        if (scoreA > scoreB) {
+            return 1;
+        } else if (scoreA == scoreB) {
+            return 0;
+        } else {
+            return -1;
         }
-        else if(scoreA==scoreB){
-            outcome="Draw";
-        }
-        else{
-            outcome="Team B won";
-        }
-        return outcome;
     }
 }
 
--- src/test/java/com/team/manager/engine/MatchTest.java
+++ src/test/java/com/team/manager/engine/MatchTest.java
@@ -29,12 +29,94 @@ class MatchTest {
         player2.setPlayerStats(statsP2);
         System.out.println(player1.toString());
         System.out.println(player2.toString());
-        System.out.println(match.lanerComparison(player1, player2));
-        assertEquals(2, match.lanerComparison(player1, player2));
+        System.out.println(match.comparePlayerStats(player1, player2));
+        assertEquals(2, match.comparePlayerStats(player1, player2));
     }
 
     @Test
-    void shouldReturnString() {
+    void shouldReturnDraw() {
+        Player pA1 = new Player(), pA2 = new Player(), pA3 = new Player(), pA4 = new Player(), pA5 = new Player(),
+                pB1 = new Player(), pB2 = new Player(), pB3 = new Player(), pB4 = new Player(), pB5 = new Player();
+
+        PlayerStats statsBetter = new PlayerStats(), statsWorse = new PlayerStats();
+
+        statsBetter.setMechanics(200);
+        statsBetter.setPositioning(200);
+
+
+        List<Player> TeamA = new LinkedList<>(), TeamB = new LinkedList<>();
+
+        pA1.setPlayerStats(statsWorse);
+        pA2.setPlayerStats(statsWorse);
+        pA3.setPlayerStats(statsWorse);
+        pA4.setPlayerStats(statsWorse);
+        pA5.setPlayerStats(statsWorse);
+
+        pB1.setPlayerStats(statsWorse);
+        pB2.setPlayerStats(statsWorse);
+        pB3.setPlayerStats(statsWorse);
+        pB4.setPlayerStats(statsWorse);
+        pB5.setPlayerStats(statsWorse);
+
+        TeamA.add(pA1);
+        TeamA.add(pA2);
+        TeamA.add(pA3);
+        TeamA.add(pA4);
+        TeamA.add(pA5);
+
+        TeamB.add(pB1);
+        TeamB.add(pB2);
+        TeamB.add(pB3);
+        TeamB.add(pB4);
+        TeamB.add(pB5);
+
+        assertEquals(0, match.matchResult(TeamA,TeamB));
+
+    }
+
+    @Test
+    void shouldReturnTeamAWinner() {
+        Player pA1 = new Player(), pA2 = new Player(), pA3 = new Player(), pA4 = new Player(), pA5 = new Player(),
+                pB1 = new Player(), pB2 = new Player(), pB3 = new Player(), pB4 = new Player(), pB5 = new Player();
+
+        PlayerStats statsBetter = new PlayerStats(), statsWorse = new PlayerStats();
+
+        statsBetter.setMechanics(200);
+        statsBetter.setPositioning(200);
+
+
+        List<Player> TeamA = new LinkedList<>(), TeamB = new LinkedList<>();
+
+        pA1.setPlayerStats(statsBetter);
+        pA2.setPlayerStats(statsBetter);
+        pA3.setPlayerStats(statsBetter);
+        pA4.setPlayerStats(statsWorse);
+        pA5.setPlayerStats(statsWorse);
+
+        pB1.setPlayerStats(statsBetter);
+        pB2.setPlayerStats(statsBetter);
+        pB3.setPlayerStats(statsWorse);
+        pB4.setPlayerStats(statsWorse);
+        pB5.setPlayerStats(statsWorse);
+
+        TeamA.add(pA1);
+        TeamA.add(pA2);
+        TeamA.add(pA3);
+        TeamA.add(pA4);
+        TeamA.add(pA5);
+
+        TeamB.add(pB1);
+        TeamB.add(pB2);
+        TeamB.add(pB3);
+        TeamB.add(pB4);
+        TeamB.add(pB5);
+
+        assertEquals(1, match.matchResult(TeamA,TeamB));
+
+    }
+
+    @Test
+    void shouldReturnTeamBWinner() {
         Player pA1 = new Player(), pA2 = new Player(), pA3 = new Player(), pA4 = new Player(), pA5 = new Player(),
                 pB1 = new Player(), pB2 = new Player(), pB3 = new Player(), pB4 = new Player(), pB5 = new Player();
 
@@ -70,8 +152,8 @@ class MatchTest {
         TeamB.add(pB4);
         TeamB.add(pB5);
 
-        assertEquals("Team B won", match.matchSimulation(TeamA,TeamB));
-
+        assertEquals(-1, match.matchResult(TeamA,TeamB));
 
     }
+
 }
